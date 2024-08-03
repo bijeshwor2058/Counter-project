0b5ac27b13458574dca99c963c8f6bdbf2ba14df
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2024-08-03 13:10:05 +0530

    Button click audio feature

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ let minusBtn = document.getElementById("decrement");
 let result = document.getElementById("result");
 // console.log(plusBtn);
 // console.log(minusBtn);
-
+let play = new Audio("click-151673.mp3");
 let counter = 0;
-
 plusBtn.addEventListener("click",function(){
-  
+  play.play();
   counter++;
   result.innerHTML = counter;
-
+  play.play();
   // console.log(counter);
 })
 minusBtn.addEventListener("click",function(){
   // console.log(counter);
+  play.play();
   if(counter > 0){
+
     counter--;
     result.innerHTML = counter;
   }
